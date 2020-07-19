b00cdff8913b5d68129af769ca1b89f304d6afde
clarence <2263194561@qq.com>
2020-07-19 19:55:17 +0800

    上传房源图片-习伟博

--- focus_on_rent/celery_tasks/config.py
+++ focus_on_rent/celery_tasks/config.py
@@ -3,3 +3,7 @@
 # 指定消息队列：在ihome我们使用redis数据库做消息队列
 # 我们选择redis的3号库作为消息队列
 broker_url = 'redis://127.0.0.1:6379/3'
+result_backend = 'redis://127.0.0.1:6379/4'
+
+task_serializer = 'json'
+result_serializer = 'json'
