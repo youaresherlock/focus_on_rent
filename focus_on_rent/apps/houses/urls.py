42b523188a073fb71566f0d35fa4b4c1d4b106d9
clarence <2263194561@qq.com>
2020-07-20 14:31:13 +0800

    房屋搜索-郝艳秋

--- focus_on_rent/apps/houses/urls.py
+++ focus_on_rent/apps/houses/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # 房屋数据搜索
-    path('houses',views.HousesView.as_view()),
-    path('/houses/<int:house_id>',views.DetailView.as_view()),
-
+    path('houses', views.HousesView.as_view()),
+    path('/houses/<int:house_id>', views.DetailView.as_view()),
+    path('/houses/index', views.HousesCommandView.as_view()),
 ]
