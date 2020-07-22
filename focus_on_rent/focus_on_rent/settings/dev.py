85027e1af5470a6b307787b32f4cbe50a6f58011
clarence <2263194561@qq.com>
2020-07-22 15:45:38 +0800

    冲突

--- focus_on_rent/focus_on_rent/settings/dev.py
+++ focus_on_rent/focus_on_rent/settings/dev.py
@@ -216,9 +216,7 @@ LOGGING = {
 AUTH_USER_MODEL = 'users.User'
 
 
-# 链接七牛云
+QINIU_BUCKET_NAME = "test"
 QINIU_ACCESS_KEY = 'W7C7l8WrP2inZpgs98xlK8ZWjNc2Pn4f4GAtSYBC'
 QINIU_SECRET_KEY = '7S7QE50VeKWpwyEaNI-k6Jl8rXQz6aUmFpLKjqNa'
-QINIU_BUCKET_NAME = 'itcastihome'
 QINIU_ADDRESS = 'http://qdpjw3zve.bkt.clouddn.com/'
-
